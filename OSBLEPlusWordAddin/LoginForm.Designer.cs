07aef73826e238c5cb1580f24291747a31f85c86
cdelira <carlamdelira@gmail.com>
2017-06-15 12:00:26 -0700

    Login Authentication updated with encryption and decryption

--- OSBLEPlusWordAddin/LoginForm.Designer.cs
+++ OSBLEPlusWordAddin/LoginForm.Designer.cs
@@ -31,20 +31,22 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOSBLELogin));
             this.btnSubmit = new System.Windows.Forms.Button();
             this.groupBoxCredentials = new System.Windows.Forms.GroupBox();
-            this.lblUsername = new System.Windows.Forms.Label();
-            this.lblPassword = new System.Windows.Forms.Label();
-            this.txtUsername = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
             this.chkRememberCredentials = new System.Windows.Forms.CheckBox();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.pbLogin = new System.Windows.Forms.ProgressBar();
             this.groupBoxCredentials.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(115, 155);
+            this.btnSubmit.Location = new System.Drawing.Point(144, 194);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.Size = new System.Drawing.Size(94, 29);
             this.btnSubmit.TabIndex = 0;
             this.btnSubmit.Text = "OK";
             this.btnSubmit.UseVisualStyleBackColor = true;
@@ -56,84 +58,102 @@
             this.groupBoxCredentials.Controls.Add(this.txtUsername);
             this.groupBoxCredentials.Controls.Add(this.lblPassword);
             this.groupBoxCredentials.Controls.Add(this.lblUsername);
-            this.groupBoxCredentials.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxCredentials.Location = new System.Drawing.Point(15, 15);
+            this.groupBoxCredentials.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBoxCredentials.Name = "groupBoxCredentials";
-            this.groupBoxCredentials.Size = new System.Drawing.Size(260, 100);
+            this.groupBoxCredentials.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBoxCredentials.Size = new System.Drawing.Size(325, 125);
             this.groupBoxCredentials.TabIndex = 1;
             this.groupBoxCredentials.TabStop = false;
             this.groupBoxCredentials.Text = "Credentials";
             // 
-            // lblUsername
+            // txtPassword
             // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(7, 20);
-            this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(66, 13);
-            this.lblUsername.TabIndex = 0;
-            this.lblUsername.Text = "User Name: ";
+            this.txtPassword.Location = new System.Drawing.Point(100, 59);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(216, 22);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(100, 25);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(216, 22);
+            this.txtUsername.TabIndex = 0;
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(7, 49);
+            this.lblPassword.Location = new System.Drawing.Point(9, 61);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(59, 13);
+            this.lblPassword.Size = new System.Drawing.Size(77, 17);
             this.lblPassword.TabIndex = 1;
             this.lblPassword.Text = "Password: ";
             // 
-            // txtUsername
+            // lblUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(80, 20);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(174, 20);
-            this.txtUsername.TabIndex = 0;
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(80, 47);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(174, 20);
-            this.txtPassword.TabIndex = 1;
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(9, 25);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(87, 17);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "User Name: ";
             // 
             // chkRememberCredentials
             // 
             this.chkRememberCredentials.AutoSize = true;
-            this.chkRememberCredentials.Location = new System.Drawing.Point(22, 119);
+            this.chkRememberCredentials.Location = new System.Drawing.Point(28, 149);
+            this.chkRememberCredentials.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkRememberCredentials.Name = "chkRememberCredentials";
-            this.chkRememberCredentials.Size = new System.Drawing.Size(214, 17);
+            this.chkRememberCredentials.Size = new System.Drawing.Size(284, 21);
             this.chkRememberCredentials.TabIndex = 2;
             this.chkRememberCredentials.Text = "Remember my user name and password";
             this.chkRememberCredentials.UseVisualStyleBackColor = true;
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(197, 155);
+            this.btnCancel.Location = new System.Drawing.Point(246, 194);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(94, 29);
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // pbLogin
+            // 
+            this.pbLogin.Location = new System.Drawing.Point(13, 194);
+            this.pbLogin.Name = "pbLogin";
+            this.pbLogin.Size = new System.Drawing.Size(124, 29);
+            this.pbLogin.TabIndex = 4;
+            // 
             // frmOSBLELogin
             // 
             this.AcceptButton = this.btnSubmit;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(284, 190);
+            this.ClientSize = new System.Drawing.Size(355, 238);
+            this.Controls.Add(this.pbLogin);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.chkRememberCredentials);
             this.Controls.Add(this.groupBoxCredentials);
             this.Controls.Add(this.btnSubmit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmOSBLELogin";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "OSBLE+ Login";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmOSBLELogin_FormClosed);
+            this.Load += new System.EventHandler(this.frmOSBLELogin_Load);
             this.groupBoxCredentials.ResumeLayout(false);
             this.groupBoxCredentials.PerformLayout();
             this.ResumeLayout(false);
@@ -151,5 +171,6 @@
         private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.CheckBox chkRememberCredentials;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ProgressBar pbLogin;
     }
 }
--- OSBLEPlusWordAddin/LoginForm.cs
+++ OSBLEPlusWordAddin/LoginForm.cs
@@ -1,64 +1,231 @@
-﻿using Microsoft.Office.Interop.Word;
+﻿using OSBLEPlusWordAddin;
+using Microsoft.Office.Interop.Word;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OSBLEPlusWordAddin
 {
-    public partial class frmOSBLELogin : Form
-    {        
+    internal partial class frmOSBLELogin : Form
+    {
+        /// <summary>
+        /// Name of the file that contains the encrypted password (full path and name)
+        /// </summary>
+        private string m_credPFileName;
+
+        /// <summary>
+        /// Name of the file that contains the encrypted user name (full path and name)
+        /// </summary>
+        private string m_credUFileName;
+
+        private OSBLEState m_state = null;
+
+        private static byte[] s_key = new byte[]{
+            185,204,123,80,94,243,206,55,111,36,53,197,43,58,87,197,244,6,181,81,235,
+            249,34,106,119,93,36,29,195,106,237,113
+        };
+
+        private static byte[] s_iv = new byte[]{
+            240,218,75,92,242,171,14,143,97,50,227,197,9,242,206,202
+        };
         public frmOSBLELogin()
         {
             InitializeComponent();
+
+            m_credUFileName = Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData);
+            m_credUFileName = Path.Combine(
+                m_credUFileName, "OSBLE_Word_u.dat");
+            m_credPFileName = Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData);
+            m_credPFileName = Path.Combine(
+                m_credPFileName, "OSBLE_Word_u.dat");
         }
 
         private void btnSubmit_Click(object sender, EventArgs e)
-        {            
-            string username = txtUsername.Text;            
-            string password = txtPassword.Text;            
-            bool rememberCredentials = chkRememberCredentials.Checked;
-
-            //authenticate the username and password
-            string authkey = OSBLEAuthentication.Login(username, password);
-            //var authkey = await task; //authkey stored locally and used for authentication
-
-            if(String.IsNullOrEmpty(authkey)) //user credentials were not authenticated
+        {
+            // Ignore the click if we have an empty user name or password
+            if (0 == txtUsername.TextLength)
             {
-                //TODO: make this feedback show on the form instead of messagebox
-                MessageBox.Show("Invalid user name or password.");
+                txtUsername.Focus();
+                return;
             }
-            else if(authkey == "ConnectionFailure")
+            if (0 == txtPassword.TextLength)
             {
-                //TODO: make this feedback show on the form instead of messagebox
-                MessageBox.Show("Unable to connect to the OSBLE+ server. Please check your connection and try again.");
+                txtPassword.Focus();
+                return;
             }
-            else //authenticated! this is the users's authkey...
+
+            // Disable the buttons while we're processing
+            btnSubmit.Enabled = false;
+            btnCancel.Enabled = false;
+            // Also show the progress bar
+            pbLogin.Visible = true;
+
+            // Save user name and password if needed
+            if (chkRememberCredentials.Checked)
             {
-                if (rememberCredentials)
+                byte[] encUser = EncryptStringToBytes(txtUsername.Text, s_key, s_iv);
+                byte[] encPass = EncryptStringToBytes(txtPassword.Text, s_key, s_iv);
+                try
                 {
-                    //TODO: handle the local storage of the user credentials
+                    File.WriteAllBytes(m_credUFileName, encUser);
+                    File.WriteAllBytes(m_credPFileName, encPass);
+                }
+                catch (Exception) { }
+            }
+            else // Otherwise delete save files
+            {
+                if (File.Exists(m_credUFileName)) { File.Delete(m_credUFileName); }
+                if (File.Exists(m_credPFileName)) { File.Delete(m_credPFileName); }
+            }
+
+            m_state = new OSBLEState(txtUsername.Text, txtPassword.Text);
+            m_state.RefreshAsync(this.LoginAttemptCompleted_CT);
+        }
+
+        public OSBLEState DoPrompt()
+        {
+            if (this.ShowDialog() != DialogResult.OK)
+            {
+                return null;
+            }
+
+            return m_state;
+        }
+
+        private static string LoadEncrypted(string fileName)
+        {
+            // Load encrypted file
+            try
+            {
+                byte[] fileBytes = File.ReadAllBytes(fileName);
+                if (null == fileBytes || fileBytes.Length < 6)
+                {
+                    File.Delete(fileName);
+                    return null;
                 }
 
-                //TODO: change logged in icon
+                return DecryptStringFromBytes(fileBytes, s_key, s_iv);
+            }
+            catch (Exception ex) { return null; }
+        }
 
-                //TODO: show the course options group (currently visible, needs to default hidden)
+        private void LoginAttemptCompleted(object sender, EventArgs e)
+        {
+            pbLogin.Visible = false;
+            btnSubmit.Enabled = true;
+            btnCancel.Enabled = true;
 
-                //TODO: store the authkey locally for use in submitting data to the server
-                //MessageBox.Show(authkey);                 
-                this.Close();
-            }            
+            OSBLEStateEventArgs oe = e as OSBLEStateEventArgs;
+            if (!oe.Success)
+            {
+                MessageBox.Show(this, oe.Message, "OSBLE Login");
+                return;
+            }
+
+            this.DialogResult = DialogResult.OK;
+
+            this.Close();
+        }
+
+        // CT = cross-thread
+        private void LoginAttemptCompleted_CT(object sender, EventArgs e)
+        {
+            this.Invoke(new EventHandler(LoginAttemptCompleted), sender, e);
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(m_credUFileName))
+            {
+                // No credentials file means that they didn't save their user 
+                // name and password last time.
+                chkRememberCredentials.Checked = false;
+                return;
+            }
+
+            chkRememberCredentials.Checked = true;
+
+            string userName = LoadEncrypted(m_credUFileName);
+            if (string.IsNullOrEmpty(userName))
+            {
+                return;
+            }
+            txtUsername.Text = userName;
+
+            // Now the password
+            if (File.Exists(m_credPFileName))
+            {
+                string password = LoadEncrypted(m_credPFileName);
+                if (string.IsNullOrEmpty(password))
+                {
+                    return;
+                }
+                txtPassword.Text = password;
+            }
+            else
+            {
+                txtPassword.Text = string.Empty;
+            }
+        }
+
+        #region Cryptography methods from http://msdn.microsoft.com/en-us/library/system.security.cryptography.rijndaelmanaged.aspx
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments. 
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+            byte[] encrypted;
+            // Create an RijndaelManaged object 
+            // with the specified key and IV. 
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for encryption. 
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            // Return the encrypted bytes from the memory stream. 
+            return encrypted;
+
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
 
         private void frmOSBLELogin_FormClosed(object sender, FormClosedEventArgs e)
         {
@@ -69,6 +236,59 @@ namespace OSBLEPlusWordAddin
             //var temp2 = currentDocument.Content.Underline;
             //var temp3 = currentDocument.Content.SpellingErrors;
             //MessageBox.Show("TEST");
+        }
+
+
+
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments. 
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            // Declare the string used to hold 
+            // the decrypted text. 
+            string plaintext = null;
+
+            // Create an RijndaelManaged object 
+            // with the specified key and IV. 
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for decryption. 
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream 
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+        #endregion
+
+        private void frmOSBLELogin_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
